6edb2199cadd7c8fc6a9b9d0dff4974e0d5e040c
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-11-24 21:41:24 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header />
       <div className="px-5 pt-6">
         <Search />
@@ -13,7 +14,19 @@ const Home = () => {
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
-    </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner.png"
+          alt="Até 30% de desconto em pizza"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
+    </>
   );
 };
 
